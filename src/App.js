96172cad7c326dc751f6ff36f823ff84d0345b59
MilosTanaskovic <milos.tanaskovic@svenskadomaner.se>
2020-09-03 18:08:09 +0200

    set up - recipe app with ReactJS using API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
+import React,{useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+const App = () => {
+
+  const APP_ID = "cbc672ab";
+  const APP_KEY = "7c4b13d4370fd8c8415af5c2eed1b1c7";
+
+  useEffect( () =>{
+    getRecipes();
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`);
+    const data = await response.json();
+    console.log(data);
+  }
+
+  return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/*Create Form */}
+      <h1>Hello React</h1>
+      <form className="search-form" action="">
+        <input className="search-bar" type="text"/>
+        <button className="search-button" type="submit">Search</button>
+      </form>
+      
     </div>
   );
 }
